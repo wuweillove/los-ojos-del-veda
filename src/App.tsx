f4c6071f40daa5a91536d1a95a474cec84b9526f
wuweillove <125422883+wuweillove@users.noreply.github.com>
2026-06-19 17:08:06 -0500

    Physical migration: adding 3D Brahmanda route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Calculator from "./pages/Calculator";
 import GrahaSheet from "./pages/GrahaSheet";
+import Brahmanda3D from "./pages/Brahmanda3D";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/calcular" element={<Calculator />} />
       <Route path="/grahas/:slug" element={<GrahaSheet />} />
+      <Route path="/3d" element={<Brahmanda3D />} />
     </Routes>
   );
 }

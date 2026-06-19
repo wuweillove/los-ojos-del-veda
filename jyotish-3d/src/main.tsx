7e73017df19888a7afaf5381b6bce46cd21df5e8
wuweillove <125422883+wuweillove@users.noreply.github.com>
2026-06-19 17:15:46 -0500

    Physical migration: force pushing jyotish-3d structure and zosite configs

---
+++ jyotish-3d/src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element not found");
+createRoot(container).render(<App />);
